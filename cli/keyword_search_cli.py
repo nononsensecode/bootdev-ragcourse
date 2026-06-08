2b930f91267888387a181f7f597f65d70b01b829
kaushik <kaushikam@gmail.com>
2026-06-08 14:48:01 +0530

    tf completed

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,162 +1,106 @@
+#!/usr/bin/env python3
+
 import argparse
-import json
-import string
-from nltk.stem import PorterStemmer
-import pickle
-from pathlib import Path
-from collections import Counter, defaultdict
 
-
-class InvertedIndex:
-    def __init__(self) -> None:
-        self.index: dict[str, set[int]] = {}
-        self.docmap: dict[int, dict] = {}
-        self.term_frequencies: dict[int, Counter] = defaultdict(Counter)
-        self.tokenizer = Tokenizer()
-
-    def __add(self, doc_id: int, text: str) -> None:
-        tokens = self.tokenizer.tokenize(text)
-        for token in tokens:
-            self.index.setdefault(token, set()).add(doc_id)
-            self.term_frequencies[doc_id][token] += 1
-
-    def get_documents(self, term: str) -> list[int]:
-        return sorted(self.index.get(term.lower(), []))
-
-    def get_tf(self, doc_id: int, term: str) -> int:
-        return self.term_frequencies.get(doc_id, Counter())[term]
-
-    def build(self):
-        with open("data/movies.json") as file:
-            movies = json.load(file)["movies"]
-            for movie in movies:
-                self.docmap[int(movie["id"])] = movie
-                self.__add(int(movie["id"]), f"{movie["title"]} {movie["description"]}")
-
-    def save(self):
-        Path("cache").mkdir(parents=True, exist_ok=True)
-        with open("cache/index.pkl", "wb") as file:
-            pickle.dump(self.index, file)
-
-        with open("cache/docmap.pkl", "wb") as file:
-            pickle.dump(self.docmap, file)
-
-        with open("cache/term_frequencies.pkl", "wb") as file:
-            pickle.dump(self.term_frequencies, file)
-
-    def load(self):
-        try:
-            with open("cache/index.pkl", "rb") as file:
-                self.index = pickle.load(file)
-        except Exception as e:
-            print(f"error occurred: {str(e)}")
-
-        try:
-            with open("cache/docmap.pkl", "rb") as file:
-                self.docmap = pickle.load(file)
-        except Exception as e:
-            print(f"error occurred: {str(e)}")
-
-        try:
-            with open("cache/term_frequencies.pkl", "rb") as file:
-                self.term_frequencies = pickle.load(file)
-        except Exception as e:
-            print(f"error occurred: {str(e)}")
-
-
-class Tokenizer:
-    def __init__(self) -> None:
-        with open("data/stop_words.txt", "r") as file:
-            content = file.read()
-            self.stop_words = content.splitlines()
-        self.table = str.maketrans("", "", string.punctuation)
-        self.stemmer = PorterStemmer()
-
-    def tokenize(self, keywords: str) -> list[str]:
-        tokens = []
-        for token in keywords.split():
-            if not token:
-                continue
-            token = self.stem(
-                self._remove_punctuation(
-                    self._remove_stop_words(self._make_case_insensitive(token.lower()))
-                )
-            )
-            if token:
-                tokens.append(token)
-        return tokens
-
-    def stem(self, word: str) -> str:
-        return self.stemmer.stem(word, to_lowercase=True)
-
-    def _remove_stop_words(self, word: str) -> str:
-        return " ".join([w for w in word.split() if w not in self.stop_words])
-
-    def _make_case_insensitive(self, word: str) -> str:
-        return word.lower()
-
-    def _remove_punctuation(self, word: str) -> str:
-        return word.translate(self.table)
-
-
-def tokenize_single_term(term: str) -> str:
-    tokenizer = Tokenizer()
-    tokens = tokenizer.tokenize(term)
-    if len(tokens) == 1:
-        raise Exception("There should be exactly one token")
-    return tokens[0]
-
-
-def search(keyword: str) -> dict[int, str]:
-    tokenizer = Tokenizer()
-    inverted_index = InvertedIndex()
-    inverted_index.load()
-    if not inverted_index.index:
-        raise Exception("index is not built!!")
-    movies = {}
-    words = sorted(tokenizer.tokenize(keyword))
-    for word in words:
-        doc_ids = inverted_index.get_documents(word)
-        for doc_id in doc_ids:
-            if len(movies) == 5:
-                return movies
-            movie = inverted_index.docmap[doc_id]
-            movies[doc_id] = movie["title"]
-
-    return movies
+from lib.keyword_search import (
+    bm25_idf_command,
+    bm25_tf_command,
+    bm25search_command,
+    build_command,
+    idf_command,
+    search_command,
+    tf_command,
+    tfidf_command,
+)
+from lib.search_utils import BM25_B, BM25_K1
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
+    subparsers.add_parser("build", help="Build the inverted index")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
-    subparsers.add_parser("build", help="Build index and docmap")
-
-    tf_parser = subparsers.add_parser("tf", help="Find term frequencies")
+    tf_parser = subparsers.add_parser(
+        "tf", help="Get term frequency for a given document ID and term"
+    )
     tf_parser.add_argument("doc_id", type=int, help="Document ID")
-    tf_parser.add_argument("term", type=str, help="Term")
+    tf_parser.add_argument("term", type=str, help="Term to get frequency for")
+
+    idf_parser = subparsers.add_parser(
+        "idf", help="Get inverse document frequency for a given term"
+    )
+    idf_parser.add_argument("term", type=str, help="Term to get IDF for")
+
+    tf_idf_parser = subparsers.add_parser(
+        "tfidf", help="Get TF-IDF score for a given document ID and term"
+    )
+    tf_idf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tf_idf_parser.add_argument("term", type=str, help="Term to get TF-IDF score for")
+
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument(
+        "term", type=str, help="Term to get BM25 IDF score for"
+    )
+
+    bm25_tf_parser = subparsers.add_parser(
+        "bm25tf", help="Get BM25 TF score for a given document ID and term"
+    )
+    bm25_tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    bm25_tf_parser.add_argument("term", type=str, help="Term to get BM25 TF score for")
+    bm25_tf_parser.add_argument(
+        "k1", type=float, nargs="?", default=BM25_K1, help="Tunable BM25 K1 parameter"
+    )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Tunable BM25 b parameter"
+    )
+
+    bm25search_parser = subparsers.add_parser(
+        "bm25search", help="Search movies using full BM25 scoring"
+    )
+    bm25search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
 
     match args.command:
         case "build":
-            inverted_index = InvertedIndex()
-            inverted_index.build()
-            inverted_index.save()
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case "search":
-            # print the search query here
-            print(f"Searching for: {args.query}")
-            movies = search(args.query)
-            for id, movie in movies.items():
-                print(f"{id}: {movie}")
+            print("Searching for:", args.query)
+            results = search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. ({res['id']}) {res['title']}")
         case "tf":
-            inverted_index = InvertedIndex()
-            inverted_index.load()
-            counts = inverted_index.term_frequencies.get(args.doc_id, Counter())
-            return print(counts[args.term])
+            tf = tf_command(args.doc_id, args.term)
+            print(f"Term frequency of '{args.term}' in document '{args.doc_id}': {tf}")
+        case "idf":
+            idf = idf_command(args.term)
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            tf_idf = tfidf_command(args.doc_id, args.term)
+            print(
+                f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}"
+            )
+        case "bm25idf":
+            bm25idf = bm25_idf_command(args.term)
+            print(f"BM25 IDF score of '{args.term}': {bm25idf:.2f}")
+        case "bm25tf":
+            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1)
+            print(
+                f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25tf:.2f}"
+            )
+        case "bm25search":
+            print("Searching for:", args.query)
+            results = bm25search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. ({res['id']}) {res['title']} - Score: {res['score']:.2f}")
         case _:
             parser.print_help()
 
